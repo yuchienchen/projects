23c71e371adfd60fb311185a5890b0954f671ffb
yuchienchen <yuchienc@yahoo.com>
2024-09-25 12:06:12 -0500

    Generalizing over computational processes

--- hw02.py
+++ hw02.py
@@ -31,43 +31,48 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    def product_identity(n):
-        """Product the first N natural numbers
-        >>> product_identity(5)
-        120
-        """
-        total, k = 0, 1
-        while k <= n:   
-            total, k = total * k, k + 1
-        return total 
+    total, x = 0, 1
+    while x <= n:
+        total, x = total * term(x), x + 1
+    return total
+
+def product_identity(n):
+    """Product the first N natural numbers
+    >>> product_identity(5)
+    120
+    """
+    total, x = 0, 1
+    while x <= n:   
+        total, x = total * x, x + 1
+    return total 
         
-    def product_square(n):
-        """Product the first N squares of natural numbers
-        >>> product_square(3)
-        36
-        """
-        total, k = 0, 1
-        while k <= n:
-            total = total * pow(k, 2), k + 1
-        return total 
+def product_square(n):
+    """Product the first N squares of natural numbers
+    >>> product_square(3)
+    36
+    """
+    total, x = 0, 1
+    while x <= n:
+        total = total * pow(x, 2), x + 1
+    return total 
     
-    def product_triple(n):
-        """Product the first N triples of natural numbers
-        >>> product_triple(3)
-        162"""
-        total, k = 0, 1
-        while k <= n:
-            total = total * (3 * k), k + 1
-        return total
+def product_triple(n):
+    """Product the first N triples of natural numbers
+    >>> product_triple(3)
+    162"""
+    total, x = 0, 1
+    while x <= n:
+        total = total * (3 * x), x + 1
+    return total
     
-    def product_increment(n):
-        """Product the first N increment of natual numbers
-        >>>product_increment(3)
-        24"""
-        total, k = 0, 1
-        while k <= n:
-            total = total * (k + 1), k + 1
-        return total 
+def product_increment(n):
+    """Product the first N increment of natual numbers
+    >>>product_increment(3)
+    24"""
+    total, x = 0, 1
+    while x <= n:
+        total = total * (x + 1), x + 1
+    return total 
 
 
 def accumulate(fuse, start, n, term):
